9a68bdc9f4f550d4aa03f80a8e6a96aa50a2f767
hexachordal <francois.harewood@gmail.com>
2021-07-30 11:01:38 -0500

    commit on master

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ let q = document.getElementsByClassName("question");
 for(let i = 0; i < q.length; i++){
     q[i].onclick = function accordion(){
         let acc = q[i].firstElementChild
-        if(acc.style.display === "none"){
-            acc.style.display = "block"
-        }   else{
-                acc.style.display = "none"
+        if(acc.style.animationName !== "appear"){
+            acc.style.animationName = "appear"
+            console.log(acc.style.animationName)
+        }   else if(acc.style.animationName !== "disappear"){
+                acc.style.animationName = "disappear"
+                console.log(acc.style.animationName)
 
         }
         
