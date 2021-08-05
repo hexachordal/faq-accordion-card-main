78ea9023d52b81b408fb75460600a07d16a4dbc2
hexachordal <francois.harewood@gmail.com>
2021-08-05 18:37:14 -0500

    commit on master

--- script.js
+++ script.js
@@ -2,21 +2,23 @@
 let q = document.getElementsByClassName("question");
 
 
-for(let i = 0; i < q.length; i++){
-    q[i].onclick = function accordion(){
+for (let i = 0; i < q.length; i++) {
+    q[i].onclick = function accordion() {
         let acc = q[i].lastElementChild
         let arr = q[i].firstElementChild
-        if(acc.style.animationName !== "appear"){
+        if (acc.style.animationName !== "appear") {
             acc.style.animationName = "appear"
             arr.style.animationName = "arrDown"
-            
-        }   else if(acc.style.animationName !== "disappear"){
-                acc.style.animationName = "disappear"
-                arr.style.animationName = "arrUp"
-                
+            q[i].style.fontWeight = "700"
+
+        } else if (acc.style.animationName !== "disappear") {
+            acc.style.animationName = "disappear"
+            arr.style.animationName = "arrUp"
+            q[i].style.fontWeight = "400"
+
 
         }
-        
-        
+
+
     };
 }
